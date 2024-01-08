52126c5c758766398589cbd50e92b8395257e96d
fchirono <fchirono@gmail.com>
2024-01-08 13:31:33 -1000

    Adds dummy function for fluctuation strength calculation

---
+++ mosqito/sq_metrics/fluctuation_strength/fluctuation_strength/fluctuation_strength.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Base function for fluctuation strength calculation
+
+Author:
+    Fabio Casagrande Hirono
+    Jan 2024
+"""
+
+import numpy as np
+
+from mosqito.sq_metrics.fluctuation_strength.utils import _create_am_cosine, _create_am_noise, _create_fm_cosine
+
+
+def fluctuation_strength():
+    """
+    Dummy function for calculating fluctuation strength.
+    """
+    
+    return 0
