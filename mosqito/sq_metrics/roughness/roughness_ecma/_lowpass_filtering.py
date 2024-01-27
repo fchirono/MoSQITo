f152568d4b0b1ab685e607afc132e6fd207981cd
fchirono <fchirono@gmail.com>
2024-01-26 14:14:26 -1000

    Adds '_lowpass_filtering'

---
+++ mosqito/sq_metrics/roughness/roughness_ecma/_lowpass_filtering.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Implements the lowpass filtering in Section 7.1.7 of ECMA-418-2 (2nd Ed, 2022)
+standard, used for calculating Roughness.
+
+Author:
+    Fabio Casagrande Hirono
+    Jan 2024
+"""
+
+import numpy as np
+
+def _lowpass_filtering(R_hat, fs_50):
+    """
+    Implements the lowpass filtering in Section 7.1.7 of ECMA-418-2 (2nd Ed,
+    2022) standard for calculating Roughness. It uses different time constants
+    for the rising and falling slopes, as the perception of sound events rises
+    quickly with the beginning of the event but decays slowly with the event
+    end.
+    
+    Parameters
+    ----------
+    R_hat : (53, Nt)-shaped numpy.array
+        Array of transformed, calibrated values.
+    
+    fs_50 : float
+        Sampling frequency of interpolated calculations. Must be 50 Hz.
+    
+    Returns
+    -------
+    R_spec : (53, Nt)-shaped numpy.array
+        Array of time-dependent specific roughness.
+    """
+    
+    # find indices where R_hat increases or remains the same over time (e.g.
+    # rising slopes)
+    R_rising = np.zeros(R_hat.shape).astype(bool)
+    R_rising[:, 1:] = (np.diff(R_hat, axis=-1) >= 0)
+    
+    # Find indices where R_hat decreases over time (e.g. falling slopes)
+    R_falling = np.logical_not(R_rising)
+    
+    # filtering time constants (Eq. 110)
+    tau = np.zeros(R_hat.shape)
+    tau[R_rising] = 0.0625
+    tau[R_falling] = 0.5000
+    
+    # time-dependent specific roughness (Eq. 109)
+    exp_ = np.exp(-1 / ( fs_50 * tau ) )
+    
+    R_spec = np.zeros(R_hat.shape)
+    
+    # l_50 = 0
+    R_spec[:, 0] = R_hat[:, 0]
+    
+    # l_50 >= 1
+    R_spec[:, 1:] = ( R_hat[:, 1:] * (1 - exp_[:, 1:])
+                     + R_hat[:, 0:-1] * exp_[:, 1:] )
+    
+    return R_spec
+            
--- mosqito/sq_metrics/roughness/roughness_ecma/roughness_ecma.py
+++ mosqito/sq_metrics/roughness/roughness_ecma/roughness_ecma.py
@@ -29,15 +29,11 @@ from mosqito.sq_metrics.roughness.roughness_ecma._est_fund_mod_rate import _est_
 from mosqito.sq_metrics.roughness.roughness_ecma._weight_low_mod_rates import _weight_low_mod_rates
 from mosqito.sq_metrics.roughness.roughness_ecma._interpolate_to_50Hz import _interpolate_to_50Hz
 from mosqito.sq_metrics.roughness.roughness_ecma._nonlinear_transform import _nonlinear_transform
-
+from mosqito.sq_metrics.roughness.roughness_ecma._lowpass_filtering import _lowpass_filtering
 
 def roughness_ecma(signal, fs, sb=16384, sh=4096):
-    """[*WARNING*] Roughness calculation of a signal sampled at 48 kHz,
-    according to ECMA-418-2, 2nd Ed (2022).
-
-    *************************** WARNING! ************************************
-    * The code is not finished yet, and does not return correct results!    *    
-    *************************************************************************
+    """Roughness calculation of a signal sampled at 48 kHz, according to 
+    ECMA-418-2, 2nd Ed (2022).
 
     Parameters
     ----------
@@ -250,37 +246,7 @@ def roughness_ecma(signal, fs, sb=16384, sh=4096):
     # Perform nonlinear transform and calibration (Eqs. 104 to 108)
     R_hat = _nonlinear_transform(R_est)
     
-    
-    # -----------------------------------------------------------------------
-    # Perform lowpass filtering (smoothing) using different time constants
-    # for the rising and falling slopes - the perception of sound events rises
-    # quickly with the beginning of the event, but decays slowly with the event
-    # end.
-    
-    # find indices where R_hat increases or remains the same over time (e.g.
-    # rising slopes)
-    R_rising = np.zeros(R_hat.shape).astype(bool)
-    R_rising[:, 1:] = (np.diff(R_hat, axis=-1) >= 0)
-    
-    # Find indices where R_hat decreases over time (e.g. falling slopes)
-    R_falling = np.logical_not(R_rising)
-    
-    # filtering time constants (Eq. 110)
-    tau = np.zeros(R_hat.shape)
-    tau[R_rising] = 0.0625
-    tau[R_falling] = 0.5000
-    
-    # time-dependent specific roughness (Eq. 109)
-    exp_ = np.exp(-1 / ( fs_50 * tau ) )
-    
-    R_spec = np.zeros(R_hat.shape)
-    
-    # l_50 = 0
-    R_spec[:, 0] = R_hat[:, 0]
-    
-    # l_50 >= 1
-    R_spec[:, 1:] = ( R_hat[:, 1:] * (1 - exp_[:, 1:])
-                     + R_hat[:, 0:-1] * exp_[:, 1:] )
+    R_spec = _lowpass_filtering(R_hat, fs_50)
     
     # calculate time-dependent Roughness by integrating over all critical bands
     # with delta_z = 0.5
