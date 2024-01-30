63d59329acf2b90b48d1bf8c1592ad270ad39246
fchirono <fchirono@gmail.com>
2024-01-30 11:36:33 -1000

    Changes plot formatting

--- validations/sq_metrics/roughness_ecma/validation_roughness_ecma.py
+++ validations/sq_metrics/roughness_ecma/validation_roughness_ecma.py
@@ -4,7 +4,7 @@ Validation script of Roughness ECMA 418-2, 2nd Ed (2022) standard [1].
 
 Creates a series of amplitude-modulated sine waves at varying carrier
 frequencies 'fc' and modulation frequencies 'fm', and compares with values
-taken from Fastl & Zwicker [2], Figs. 11.1 and 11.2.
+taken from Fastl & Zwicker [2], Fig. 11.2.
 
 References:
     
@@ -43,52 +43,6 @@ t = np.linspace(0, T-dt, int(T*fs))
 
 spl = 60
 
-# %% Fig. 11.1
-
-# number of points in plot
-N_m = 15
-
-mod_low = 0.2
-mod_high = 1.0
-mod_degree = np.logspace(np.log10(mod_low), np.log10(mod_high), N_m)
-
-fc = 1000.
-fm = 70
-
-
-R_dw_m = np.zeros(N_m)
-R_ecma_m = np.zeros(N_m)
-
-R_FZ = mod_degree**1.6
-
-for i, m in enumerate(mod_degree):
-    
-    # modulating signal
-    xm = m*np.sin(2*np.pi*fm*t)
-
-    # test signal - amplitude-modulated sine wave
-    x = _create_am_sin(spl, fc, xm, fs)
-    
-    # calculate Roughness using DW model
-    R_dw_temp, _, _, _ = roughness_dw(x, fs=fs, overlap=0.5)
-    R_dw_m[i] = np.mean(R_dw_temp)
-    
-    # calculate Roughness using ECMA model
-    R_ecma_temp, _, _, _ = roughness_ecma(x, fs)
-    R_ecma_m[i] = np.percentile(R_ecma_temp, 90)
-        
-
-plt.figure()
-plt.loglog(mod_degree, R_FZ, label='Fastl & Zwicker')
-plt.loglog(mod_degree, R_dw_m, label='MoSQITo [Daniel & Weber]')
-plt.loglog(mod_degree, R_ecma_m, label='MoSQITo [ECMA-418-2]')
-plt.xlim([0.1, 1])
-plt.ylim([0.07, 1])
-plt.legend()
-plt.xlabel(r'Degree of modulation')
-plt.ylabel('Roughness [asper]')
-plt.title('Fastl & Zwicker, Fig. 11.1')
-
 # %% Fig. 11.2 - Define ranges of fc and fm
 
 fc_all = np.array([125, 250, 500, 1000, 2000, 4000, 8000])
@@ -114,7 +68,6 @@ for i, fc in enumerate(fc_all):
     
     plt.loglog(fm, R, label=f'fc = {fc} Hz', linestyle=linestyles[i])
 
-plt.grid()
 plt.xlim([10, 400])
 plt.ylim([0.07, 1])
 plt.legend()
@@ -154,14 +107,25 @@ for i, fc in enumerate(fc_all):
         R_ecma_temp, _, _, _ = roughness_ecma(x, fs)
         R_ecma[j] = np.percentile(R_ecma_temp, 90)
         
+    # plot comparison
     plt.figure()
-    plt.loglog(fm_array, R_fz, '-', linewidth=2, label='Fastl & Zwicker [interp]')
-    plt.loglog(fm_array, R_dw, ':', linewidth=1.5, label='MoSQITo [Daniel & Weber]')
-    plt.loglog(fm_array, R_ecma, '-.', label='MoSQITo [ECMA-418-2]')
+    
+    plt.loglog(fm_array, R_ecma, 'o', color='C0', 
+               label='MoSQITo [ECMA-418-2]')
+    
+    plt.loglog(fm_array, R_dw, 's', linewidth=1.5, color='C1',
+               label='MoSQITo [Daniel & Weber]')
+
+    plt.loglog(fm_array, R_fz, '--', linewidth=2, color='0.45',
+               label='Fastl & Zwicker [interp]')
+    plt.loglog(fm_array, R_fz+0.1, ':', linewidth=1, color='0.25',
+               label='0.1 asper tolerance')
+    plt.loglog(fm_array, R_fz-0.1, ':', linewidth=1, color='0.25')
+    
     plt.legend()
-    plt.grid()
+    plt.grid(which='both')
     plt.xlim([10, 400])
-    # plt.ylim([0.07, 1])
+    plt.ylim([0.07, 1])
     plt.xlabel(r'Modulation Frequency $f_m$ [Hz]')
     plt.ylabel('Roughness [asper]')
     plt.title(rf'Carrier frequency $f_c$={fc} Hz')
